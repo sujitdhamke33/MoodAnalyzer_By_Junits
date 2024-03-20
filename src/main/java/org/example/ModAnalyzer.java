02ce2c4ff0ac527608a3e574293ba33d0ed18ab5
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-20 12:50:45 +0530

    UC1

---
+++ src/main/java/org/example/ModAnalyzer.java
@@ -0,0 +1,14 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class ModAnalyzer {
+    public String analyzeMood (String message){
+        if(message.contains("sad")){
+            return "SAD";
+        } else if (message.contains("any")) {
+            return "HAPPY";
+        }
+        return "";
+    }
+}
