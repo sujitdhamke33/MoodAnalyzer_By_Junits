c1ea063ed5b62a4f07e06b9117ec85ddad994a64
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-20 14:19:21 +0530

    ability to analyse and respond Happy or Sad Mood

--- src/main/java/org/example/modAnalyze.java
+++ src/main/java/org/example/modAnalyze.java
@@ -3,7 +3,13 @@ package org.example;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class modAnalyze {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the modanalyzer programme");
+    public String analyzeMood (String message){
+        if(message.contains("sad")){
+            return "SAD";
+        } else if (message.contains("any")) {
+            return "HAPPY";
+
+        }
+        return "";
     }
 }
