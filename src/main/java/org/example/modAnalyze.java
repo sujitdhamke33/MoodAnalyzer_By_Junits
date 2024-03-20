66795d8cdd08c2c2b09a495a2385b836c8abf799
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-20 14:16:48 +0530

    Master

---
+++ src/main/java/org/example/modAnalyze.java
@@ -0,0 +1,9 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class modAnalyze {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the modanalyzer programme");
+    }
+}
